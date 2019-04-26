f4e4ec908c61461e91aa67ea19a6e6aac8684813
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-04-26 14:22:33 +0300

    add inteface transactions

---
+++ src/app/interfaces/Transaction.ts
@@ -0,0 +1,11 @@
+export interface Transaction {
+  deadline: number;
+  fee: number;
+  mode: number;
+  remoteAccount: string;
+  signature: string;
+  signer: string;
+  timeStamp: number;
+  type: number;
+  version: number;
+}

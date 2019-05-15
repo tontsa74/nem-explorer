f053edc9cb1fe618aea84c7ccaa93df858ce1937
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-05-15 15:33:53 +0300

    install hammerja update npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log('ngOnInit');
     this.router.events.subscribe((res) => {
       this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
   });
